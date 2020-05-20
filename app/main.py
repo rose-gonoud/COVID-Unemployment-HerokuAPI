cf08c4fc5558081bf4ecb4fcac31d928712dc817
Rose <rose.gonoud@gmail.com>
2020-05-20 00:04:45 -0700

    county-level unemp data to DB and new API route

--- app/main.py
+++ app/main.py
@@ -25,8 +25,8 @@ def welcome():
 
     start_menu = """<br>
                 Explore our US unemployment data API! 
-                <br><br>
-                Follow route "/unemploymentData" for all US unemployment data from Jan 2019 through April 2020
+                <br><br><br>
+                Follow route "/unemploymentData" for all US Department of Labor unemployment data from Jan 2019 through April 2020
                 <br><br>
                 Add optional start and end date parameters by inputing dates in the form of:
                 <br>
@@ -34,7 +34,7 @@ def welcome():
                 <br><br>
                 Add an optional state filter, such as:
                 <br>
-                state=NY,AL,NJ
+                state=NY,NJ,CT
                 <br>
                 Input only two-letter capitalized state and territory abbreviations, with no spaces between list items.
                 <br><br>
@@ -43,6 +43,17 @@ def welcome():
                 Parameters left unspecified will default to the most general query possible.
                 <br>
                 Query parameters may be input in any order.
+                <br><br><br>
+                Follow route "/countyUnemploymentEstimates" to view all AGS unemployment estimates for counties, boroughs, and municipalities from March 2020 through May 2020
+                <br><br>
+                Add optional start and end date parameters by inputing dates in the form of:
+                <br>
+                start_date or end_date=yyyy-mm-dd
+                <br><br>
+                Add an optional county filter (with possible multi-selections on county FIPS codes), such as:
+                <br>
+                county_FIPS=36079,36119,36087 --> Putnam, Westchester, and Rockland, NY
+                <br>
                 """
 
     return start_menu
@@ -64,10 +75,6 @@ def unemploymentData():
         # query the max of all file_week_ended entries if no date is given in that parameter
         max_end_date = session.query(func.max(unemployment.file_week_ended))
         end_date = max_end_date
-
-    # these operators (<, >, etc) not supported between queries - how might I print custom message for incorrect ranges?
-    # if (start_date <= end_date):
-    #     return "That is not a possible date range: please make sure your start_date is earlier than your end_date"
 
     if not stateparam:
         results = session.query(unemployment).filter(unemployment.file_week_ended >= start_date).filter(unemployment.file_week_ended <= end_date)
@@ -100,6 +107,61 @@ def unemploymentData():
             "continued_claims": result.continued_claims,
             "covered_employment": result.covered_employment,
             "insured_unemployment_rate": result.insured_unemployment_rate
+        })
+
+    return jsonify(data)
+
+# --------------------------------------------------------------------------------------------------------------------------
+# save reference to county unemployment data
+county_unemployment = Base.classes.countyUnemploymentData
+
+@app.route("/countyUnemploymentEstimates")
+def countyUnemploymentData():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    countyparam = request.args.get("county_FIPS")
+
+    session = Session(engine)
+
+    if not start_date:
+        # query the min of all file_week_ended entries if no date is given in that parameter
+        min_start_date = session.query(func.min(county_unemployment.file_week_ended))
+        start_date = min_start_date
+
+    if not end_date:
+        # query the max of all file_week_ended entries if no date is given in that parameter
+        max_end_date = session.query(func.max(county_unemployment.file_week_ended))
+        end_date = max_end_date
+
+    if not countyparam:
+        results = session.query(county_unemployment).filter(county_unemployment.file_week_ended >= start_date).filter(county_unemployment.file_week_ended <= end_date)
+    
+    if countyparam:
+        print("---------------------------")
+        print("Whats in County:", countyparam)
+        print("Whats it's type:", type(countyparam))
+        print("---------------------------")
+        countyparam = countyparam.split(',')
+        print("Whats in County after split:", countyparam)
+        print("What type is it now?", type(countyparam))
+        print("---------------------------")
+
+        if isinstance(countyparam, list):
+            print("Are you making it to this line?")
+            # this should make an array of counties valid and handle the single-county case
+            results = session.query(county_unemployment).filter(county_unemployment.file_week_ended >= start_date).filter(county_unemployment.file_week_ended <= end_date).filter(county_unemployment.county_code.in_(countyparam)).all()
+
+    session.close()
+
+    data = []
+    for result in results:
+        data.append({
+            "county_code": result.county_code,
+            "county_name": result.county_name,
+            "labor_force": result.labor_force,
+            "file_week_ended": result.file_week_ended,
+            "percent_unemployed": result.percent_unemployed,
+            "total_unemployed": result.total_unemployed
         })
 
     return jsonify(data)
